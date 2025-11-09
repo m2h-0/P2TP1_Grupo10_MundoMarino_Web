a4406e61c3a0e545480dc9d01eb2714289a80892
Matías Hayashi <hayashimario@yahoo.com>
2025-11-09 01:52:59 -0300

    Primer commit

---
+++ src/main/java/com/grupo10/mundomarino/SistemaDeGestionMundoMarinoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo10.mundomarino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionMundoMarinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionMundoMarinoApplication.class, args);
+	}
+
+}
