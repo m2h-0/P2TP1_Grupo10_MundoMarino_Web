785ee541c179e1910e3ec9a55c4e119bb79d8c0e
Matías Hayashi <hayashimario@yahoo.com>
2025-11-11 16:27:51 -0300

    Implementa Itinerario

---
+++ src/main/java/com/grupo10/mundomarino/service/ItinerarioService.java
@@ -0,0 +1,14 @@
+package com.grupo10.mundomarino.service;
+
+import com.grupo10.mundomarino.entity.Itinerario;
+import java.util.List;
+import java.util.Optional;
+
+public interface ItinerarioService {
+    List<Itinerario> obtenerTodosLosItinerarios();
+    Optional<Itinerario> obtenerItinerarioPorId(Integer id);
+    Itinerario crearItinerario(Itinerario itinerario);
+    Itinerario actualizarItinerario(Integer id, Itinerario itinerario);
+    void eliminarItinerario(Integer id);
+    List<Itinerario> ordenarPorNumEspeciesDesc();
+}
